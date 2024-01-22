412bf9051b12074a4631c946188863ff36e05a4c
denis <denniiskim@gmail.com>
2024-01-22 03:57:17 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,18 @@
 class Student:
-    pass
+    def __init__(self):
+        pass
+
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for x in range(0, 10):
+            super().raise_hand()
